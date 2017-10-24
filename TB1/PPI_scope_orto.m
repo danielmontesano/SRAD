1812d00984713bcdb76377061e8735a708117a9e
Daniel Montesano <daniel.montesano@gmail.com>
2017-10-24 17:42:28 +0200

    Correccion de offset en PPI_orto

--- TB1/PPI_scope_orto.m
+++ TB1/PPI_scope_orto.m
@@ -1,25 +1,6 @@
-clear all
-close all
-[DatosPlots, directorio] = uigetfile('*mat', 'Escoja el fichero de datos digitalizados a procesar');
-load (cat(2, directorio, DatosPlots)); % los datos de plots
-%load('./muestras/C1_2017.mat');
-
-canal1=Data.Channel1;
-canal1=double(canal1);
-
-canal2=Data.Channel2;
-canal2=double(canal2);
-
-celdasAz=Data.SegmentCount;
-celdasDis=Data.RecordLength;
-bits=Data.Resolution;
-escala=Data.Escala;
-tau=Data.Tau;
-rpm=Data.Rpm;
-PRF=Data.PRF;
-fs=Data.SampleFrequency;
-
-cargaPintaOrtofoto %Funcion que carga la ortofoto
+PPIscope; %Funcion que carga la matriz radar
+close all;
+cargaPintaOrtofoto; %Funcion que carga la ortofoto
 
 Xo=ortofoto.PosicionRadar(1); %Se saca la posicion del radar
 Yo=ortofoto.PosicionRadar(2);
@@ -32,14 +13,10 @@ Yo=ortofoto.PosicionRadar(2);
 
 
 
-theta = pi*(linspace(-1,1,length(canal1(1,:)))); %Se crea un vector de longitud el numero de pulsos que recorra los 360 grados
-r = linspace(0,escala*1851,length(canal1(:,1))); %se crea un vector de longitud el numero de muestras por periodo y de valor maximo el fondo de escala
-
 %pasamos a coordenadas polares
-X = r'*cos(theta)+Xo;%Se le suma el offset de la posicion del radar
-Y = r'*sin(theta)+Yo;%Se le suma el offset de la posicion del radar
-C = -canal1;
-figure(4);
+X = r'*cos(theta-Azimut)+Xo;%Se le suma el offset de la posicion del radar
+Y = r'*sin(theta-Azimut)+Yo;%Se le suma el offset de la posicion del radar
+C = -flip(canal1')';
 h=pcolor(X,Y,C);
 axis equal;
 set(h, 'EdgeColor', 'none');
