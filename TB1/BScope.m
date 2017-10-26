d09e33b1df13d15a56d816706c884e899c96e07b
hcuevases <hcuevases@users.noreply.github.com>
2017-10-26 16:14:16 +0200

    Mierda fuera

--- TB1/BScope.m
+++ TB1/BScope.m
@@ -47,20 +47,3 @@ axis([0 inf 0 inf])
 xlabel('Grados')
 ylabel('Distancia (m)')
 colorbar
-info(1) = {'Frecuencia de muestreo:'};
-info(2)= {fs};
-info(3) = {'Celdas Azimut:'};
-info(4) ={celdasAz};
-info(5) = {'Celdas Distancia:'};
-info(6)={celdasDis};
-info(7) = {'Escala:'};
-info(8)={escala};
-annotation('textbox', [.8 .5 .3 .3], 'String', info,'FitBoxToText','on');
-
-% figure(3)
-% x=linspace(0,360,length(canal1(1,:)));
-% y=linspace(0,escala*1852,length(canal1(:,1)));
-% z=-canal1;
-% h=pcolor(x,y,z)
-% set(h, 'EdgeColor', 'none');
-% colorbar
