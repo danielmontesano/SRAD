a9a69ad1b3e10bdf69b4875e24c0ab7faa3e6668
ivaniturat <33063228+ivaniturat@users.noreply.github.com>
2018-01-16 11:31:02 +0100

    Cancelador

---
+++ TB3/cancelador.m
@@ -0,0 +1,30 @@
+function [orden, MatrizRadar] = cancelador(MatrizCancelada)
+%CANCELADOR Simple, Doble o Tripe
+%
+
+
+if orden == 1
+    
+    coefs = [0.5 -0.5];
+    MatrizCancelada = filter(coefs,1,MatrizRadar);
+    Matriz_Cancelada = Matriz_Cancelada(:,1:end); 
+    
+elseif orden == 2
+    
+    coefs = [0.25 -0.5 0.25];
+    MatrizCancelada = filter(coefs,1,MatrizRadar);
+    Matriz_Cancelada = Matriz_Cancelada(:,2:end); 
+    
+elseif orden == 3
+    
+    coefs = [0.125 -0.375 0.375 -0.125]
+    MatrizCancelada = filter(coefs,1,MatrizRadar);
+    Matriz_Cancelada = Matriz_Cancelada(:,4:end); 
+    
+else
+    
+    MatrizCancelada = MatrizRadar;
+end
+    
+end
+
