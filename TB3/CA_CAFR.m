8bf025c1c44345f93852c0166c2f814e5e0a3832
Daniel Montesano <daniel.montesano@gmail.com>
2018-01-19 16:56:11 +0100

    CAFRE

--- TB3/CA_CAFR.m
+++ TB3/CA_CAFR.m
@@ -4,13 +4,14 @@ m = 4;
 g = 2;
 % Matriz= zeros(3, size(MatrizEntrada,2));
 % Matriz2= [Matriz;MatrizEntrada];
-Matriz2=MatrizEntrada(4:end,:);
-Vec = zeros(1,15)';
-Vec(8) =1;
+% Matriz2=MatrizEntrada(4:end,:);
+% Vec = zeros(1,15)';
+% Vec(8) = 1;
 CA_CFAR_filter = [ones(1,m/2) zeros(1,g/2) 0 zeros(1,g/2) ones(1,m/2)];
 
 Umbral = filter(CA_CFAR_filter,1,MatrizEntrada,[],1);
-% Umbral =Umbral(4:end-4,:);
+Umbral = Umbral(7:end,:); %Se recorta 6 muestras iniciales
+MatrizEntrada = MatrizEntrada(4:end-3,:); %Se recortan 3 por cada lado
 
 %%
 
@@ -18,7 +19,7 @@ FactorDeEscala = 1
 
 if(FactorDeEscala)
 k = 0;
-for T=.1.1:6
+for T=.1:.01:4
    k = k+1;
    Pfa(k) = mean(mean(MatrizEntrada>T*Umbral));
    T_vec(k) = T;
@@ -34,7 +35,7 @@ ylabel('Log1(Pfa)')
 end
 %%
 figure(11)
-imagesc((MatrizEntrada>0.4*Umbral))
+imagesc((MatrizEntrada>0.28*Umbral))
 set(gca, 'YDir', 'normal');
 colormap('jet')
 c=colorbar;
